3dc8423f8928b60510ebd67babbdfaf1faa106f3
diltz <38502930+Diltz@users.noreply.github.com>
2023-11-18 05:07:47 +0700

    Spotify support!

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,3 +1,4 @@
+import { PlaylistData, SpotifyClient, TrackInfo } from 'node-spotify-web'
 import ytdl from 'ytdl-core'
 import ytpl from 'ytpl'
 import ytsr from 'diltz-ytsr-fixed'
@@ -9,6 +10,14 @@ import {Database} from 'better-sqlite3'
 
 const next_track = require("../util/next-track.js")
 const clear_queue = require("../util/clear-queue.js")
+
+const SPOTIFY_TRACK_REGEX = /^https:\/\/open\.spotify\.com\/track\/[A-Za-z0-9]+\?si=[A-Za-z0-9]+$/;
+const SPOTIFY_PLAYLIST_REGEX = /^https:\/\/open\.spotify\.com\/playlist\/[A-Za-z0-9]+\?si=[A-Za-z0-9]+$/;
+
+// create spotify client
+
+const Spotify = new SpotifyClient(process.env.spotify_clientid as string, process.env.spotify_secret as string)
+Spotify.authenticate([])
 
 //
 
@@ -45,8 +54,6 @@ async function execute(client: Client, interaction: any, db: Database) {
 
     await interaction.deferReply()
 
-    //
-
     let url = search.replace(/'/g, "''")
 
     // support for playlists
@@ -61,8 +68,7 @@ async function execute(client: Client, interaction: any, db: Database) {
             playlist = await ytpl(url, {pages: 0, limit: 50})
         } catch (error) {
             console.log(error);
-
-            return interaction.editReply({content: 'Не удалось получить информацию о плейлисте'})
+            return interaction.editReply({content: 'Не удалось получить информацию о плейлисте :x:'})
         }
 
         // remove live tracks
@@ -71,20 +77,76 @@ async function execute(client: Client, interaction: any, db: Database) {
 
         // add tracks to queue
 
-        playlist.items.forEach((item: any, index: number) => {
-            // get position
-
-            const position = db.prepare("SELECT * FROM queue WHERE guild_id = ?").all(guild.id).length + index + 1
-
+        playlist.items.forEach((item: any) => {
             // add track to queue
 
             db.prepare("INSERT INTO queue (guild_id, search, name, requested, channelid) VALUES (?, ?, ?, ?, ?)").run(guild.id, item.shortUrl, item.title, member.user.id, interaction.channel.id)
         })
 
-        await interaction.editReply({content: `Добавлено ${playlist.items.length} треков в очередь`})
+        await interaction.editReply({content: `Добавлено ${playlist.items.length} треков в очередь :white_check_mark:`})
         
         if (!getVoiceConnection(guild.id)) {
-            next_track(guild, db, interaction.channel)
+            try {
+                next_track(guild, db, interaction.channel)
+            } catch (error) {
+                console.error(`Issue occured in ${guild.id}\nError: ${error}`)
+            }
+        }
+
+        return
+    }
+
+    // spotify
+
+    if (SPOTIFY_TRACK_REGEX.test(url)) { // track link
+        const id = url.split('/')[4].split('?si')[0]
+        let result: TrackInfo
+
+        try {
+            result = await Spotify.getTrackInfo(id) as TrackInfo
+        } catch {
+            return interaction.editReply({content: `Не удалось получить информацию о треке :x:`})
+        }
+
+        url = `${result.name} - ${result.artists.join(', ')}`
+        await interaction.editReply({content: `<:spotify:1175190471002312786> Получен трек **${url}**. Поиск.. <a:load:1175190469534298225>`})
+    } else if (SPOTIFY_PLAYLIST_REGEX.test(url)) { // playlist link
+        const id = url.split('/')[4].split('?si')[0]
+        let result: PlaylistData
+        let tracks: {name: string, url: string}[] = []
+
+        try {
+            result = await Spotify.getPlaylist(id) as PlaylistData
+        } catch {
+            return interaction.editReply({content: `Не удалось получить информацию о плейлисте :x:`})
+        }
+
+        await interaction.editReply({content: `<:spotify:1175190471002312786> Получен плейлист **${result.name}**. Поиск **${result.tracks.length}** треков.. <a:load:1175190469534298225>`})
+
+        // find every track in yt
+
+        for await (const track of result.tracks) {
+            let search_results = await ytsr(`${track.name} - ${track.artists.join(', ')}`, {pages: 1, limit: 1})
+            search_results.items = search_results.items.filter((item: any) => !item.isLive && item.type === 'video')
+
+            if (search_results.items.length > 0) {
+                let video: any = search_results.items[0]
+                tracks.push({name: video.title, url: video.url})
+            }
+        }
+
+        tracks.forEach((item) => {
+            db.prepare("INSERT INTO queue (guild_id, search, name, requested, channelid) VALUES (?, ?, ?, ?, ?)").run(guild.id, item.url, item.name, member.user.id, interaction.channel.id)
+        })
+
+        await interaction.editReply({content: `Добавлено ${tracks.length} треков в очередь :white_check_mark:`})
+        
+        if (!getVoiceConnection(guild.id)) {
+            try {
+                next_track(guild, db, interaction.channel)
+            } catch (error) {
+                console.error(`Issue occured in ${guild.id}\nError: ${error}`)
+            }
         }
 
         return
@@ -96,12 +158,10 @@ async function execute(client: Client, interaction: any, db: Database) {
         let search_results = await ytsr(url, {pages: 1, limit: 1})
         search_results.items = search_results.items.filter((item: any) => !item.isLive && item.type === 'video')
 
-        if (search_results.items.length === 0) {
-            return interaction.editReply({content: 'Ничего не найдено'})
+        if (search_results.items.length > 0) {
+            const video: any = search_results.items[0]
+            url = video.url
         }
-
-        const video: any = search_results.items[0]
-        url = video.url
     }
 
     // get info about track from youtube
@@ -118,13 +178,13 @@ async function execute(client: Client, interaction: any, db: Database) {
         })
     } catch (error) {
         console.log(error);
-        return interaction.editReply({content: 'Не удалось получить информацию о треке'})
+        return interaction.editReply({content: 'Не удалось получить данные о треке :x:'})
     }
 
     // check if track is live
 
     if (track_info.videoDetails.isLiveContent) {
-        return interaction.editReply({content: 'Нельзя воспроизводить трансляции'})
+        return interaction.editReply({content: 'Нельзя воспроизводить трансляции :x:'})
     }
 
     // check queue, if there any tracks in queue, add track to queue
@@ -134,11 +194,16 @@ async function execute(client: Client, interaction: any, db: Database) {
     const queue: any = db.prepare("SELECT COUNT(*) FROM queue WHERE guild_id = ?").get(guild.id)
 
     if (queue["COUNT(*)"] !== 1) {
-        return interaction.editReply({content: `Трек **${track_info.videoDetails.title}** добавлен в очередь под номером **${queue["COUNT(*)"]}**`})
+        return interaction.editReply({content: `Трек **${track_info.videoDetails.title}** добавлен в очередь под номером **${queue["COUNT(*)"]}** :white_check_mark:`})
     }
 
     interaction.editReply({content: `Запускаю трек **${track_info.videoDetails.title}**`})
-    next_track(guild, db, interaction.channel)
+
+    try {
+        next_track(guild, db, interaction.channel)
+    } catch (error) {
+        console.error(`Issue occured in ${guild.id}\nError: ${error}`)
+    }
 }
 
 module.exports = {
